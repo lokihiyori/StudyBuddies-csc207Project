6da9cfb3532b2f941d1f48cd3d009caa89183f41
Ana Ciur <ana.ciur@mail.utoronto.ca>
2024-07-10 18:58:15 -0400

    login use case

---
+++ src/usecase/LoginInputBoundary.java
@@ -0,0 +1,5 @@
+package usecase;
+
+public interface LoginInputBoundary {
+    void execute(LoginInputData loginInputData);
+}
---
+++ src/usecase/LoginInputData.java
@@ -0,0 +1,27 @@
+
+package usecase;
+
+public class LoginInputData {
+
+    final private String username;
+    final private String password;
+    final private String repeatPassword;
+
+    public LoginInputData(String username, String password, String repeatPassword) {
+        this.username = username;
+        this.password = password;
+        this.repeatPassword = repeatPassword;
+    }
+
+    String getUsername() {
+        return username;
+    }
+
+    String getPassword() {
+        return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+}
