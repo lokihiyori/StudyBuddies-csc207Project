6aa14c2d67589aee9c86876a30082b64b79434b7
aamyahanda <aamya.handa@gmail.com>
2024-07-10 18:31:02 -0400

    User entity

---
+++ src/entity/User.java
@@ -0,0 +1,17 @@
+package entity;
+
+import java.util.List;
+
+public interface User {
+
+    String getName();
+
+    String getUsername();
+
+    String getPassword();
+
+    String getEmail();
+
+    List<String> getCourses();
+}
+
