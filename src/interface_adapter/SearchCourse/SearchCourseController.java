70f2acb6f7f317c9e6c29af66fcbbd7140f72acf
Yujing Duan <yujing.duan@mail.utoronto.ca>
2024-07-18 10:28:33 -0400

    The controller for search course

--- src/interface_adapter/SearchCourse/SearchCourseController.java
+++ src/interface_adapter/SearchCourse/SearchCourseController.java
@@ -1,2 +1,18 @@
-package interface_adapter.SearchCourse;public class Controller {
+package interface_adapter.SearchCourse;
+
+import usecase.SearchCourse.SearchCourseInputData;
+import usecase.SearchCourse.SearchCourseInputBoundary;
+
+public class SearchCourseController {
+
+    private SearchCourseInputBoundary inputBoundary;
+
+    public SearchCourseController(SearchCourseInputBoundary inputBoundary) {
+        this.inputBoundary = inputBoundary;
+    }
+
+    public void handleInput(String query) {
+        inputBoundary.searchCourse(new SearchCourseInputData(query));
+    }
+
 }
