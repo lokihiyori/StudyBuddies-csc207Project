0816af746185b2b1fe3a6876b99bec108b9091dd
lokihiyori <144290962+lokihiyori@users.noreply.github.com>
2024-07-21 18:30:15 -0400

    Delete src/use_case/LeaveGroupChatOutputBoundary.java

--- src/use_case/LeaveGroupChatOutputBoundary.java
+++
@@ -1,6 +0,0 @@
-package usecase.LeaveGroupChat;
-
-public interface LeaveGroupChatOutputBoundary {
-    void onLeaveSuccess(LeaveGroupChatOutputData outputData);
-    void onLeaveFailure(LeaveGroupChatOutputData outputData);
-}
