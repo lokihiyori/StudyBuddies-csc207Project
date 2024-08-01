fd9ed9949e2611d44a4870ffbbda1272f1bab5d1
Yujing Duan <yujing.duan@mail.utoronto.ca>
2024-08-01 16:38:52 -0400

    Implement group chat API for the server side

--- src/main/java/SocketIO/GroupChatServer.java
+++ src/main/java/SocketIO/GroupChatServer.java
@@ -1,2 +1,45 @@
-package SocketIO;public class GroupChatServer {
+package SocketIO;
+
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+
+public class GroupChatServer {
+    public static void main(String[] args) {
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(3000);
+
+        SocketIOServer server = new SocketIOServer(config);
+
+        server.addConnectListener(new ConnectListener() {
+            @Override
+            public void onConnect(com.corundumstudio.socketio.SocketIOClient client) {
+                System.out.println("Client connected: " + client.getSessionId());
+            }
+        });
+
+        server.addDisconnectListener(new DisconnectListener() {
+            @Override
+            public void onDisconnect(com.corundumstudio.socketio.SocketIOClient client) {
+                System.out.println("Client disconnected: " + client.getSessionId());
+            }
+        });
+
+        server.addEventListener("message", String.class, new DataListener<String>() {
+            @Override
+            public void onData(com.corundumstudio.socketio.SocketIOClient client, String data, com.corundumstudio.socketio.AckRequest ackSender) {
+                System.out.println("Received message: " + data);
+                server.getBroadcastOperations().sendEvent("message", data);
+                System.out.println("Broadcasted message: " + data);
+            }
+        });
+
+        server.start();
+        System.out.println("Server is running on port 3000");
+        Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
+    }
 }
