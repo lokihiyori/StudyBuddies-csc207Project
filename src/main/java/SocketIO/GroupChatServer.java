63340fcea359bf1023440eaca45a701d1bf1104d
Yujing Duan <yujing.duan@mail.utoronto.ca>
2024-08-04 14:20:00 -0400

    update group chat server so that it can be called elsewhere

--- src/main/java/SocketIO/GroupChatServer.java
+++ src/main/java/SocketIO/GroupChatServer.java
@@ -48,6 +48,15 @@ public class GroupChatServer {
 
  */
 
+
+
+
+
+
+
+
+/*
+// can be executed from CourseSearchView !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 package SocketIO;
 
 import com.corundumstudio.socketio.Configuration;
@@ -93,6 +102,55 @@ public class GroupChatServer {
         System.out.println("Server is running on port 3000");
 
         // Add shutdown hook to stop the server gracefully
+        Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
+    }
+}
+
+ */
+
+package SocketIO;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+
+public class GroupChatServer {
+    private SocketIOServer server;
+
+    public void startServer(int port) {
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(port); // Use the specified port
+
+        server = new SocketIOServer(config);
+
+        server.addConnectListener(new ConnectListener() {
+            @Override
+            public void onConnect(com.corundumstudio.socketio.SocketIOClient client) {
+                System.out.println("Client connected: " + client.getSessionId());
+            }
+        });
+
+        server.addDisconnectListener(new DisconnectListener() {
+            @Override
+            public void onDisconnect(com.corundumstudio.socketio.SocketIOClient client) {
+                System.out.println("Client disconnected: " + client.getSessionId());
+            }
+        });
+
+        server.addEventListener("message", String.class, new DataListener<String>() {
+            @Override
+            public void onData(com.corundumstudio.socketio.SocketIOClient client, String data, com.corundumstudio.socketio.AckRequest ackSender) {
+                System.out.println("Received message: " + data);
+                server.getBroadcastOperations().sendEvent("message", data);
+                System.out.println("Broadcasted message: " + data);
+            }
+        });
+
+        server.start();
+        System.out.println("Server is running on port " + port);
         Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
     }
 }
