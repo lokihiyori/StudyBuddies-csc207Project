cf651fde2af20328e8e079d46e83bec842e6a1c4
lixun <xunlily.li@mail.utoronto.ca>
2024-08-04 17:50:52 -0400

    calendar event factory update

--- src/main/java/entity/CommonCalendarEventFactory.java
+++ src/main/java/entity/CommonCalendarEventFactory.java
@@ -2,10 +2,19 @@ package entity;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class CommonCalendarEventFactory implements CalendarEventFactory{
     @Override
     public CalendarEvent create(String name, LocalDate date, LocalDate endDate, LocalTime time, LocalTime endTime, String organizer, String location) {
-        return null;
+        return new CommonCalendarEvent(organizer,
+                name,
+                date,
+                time,
+                location,
+                "",
+                new ArrayList<>(),
+                endDate,
+                endTime);
     }
 }
