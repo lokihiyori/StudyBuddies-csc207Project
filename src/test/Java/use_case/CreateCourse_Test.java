5fa98cdae3d505338e771e3ed409710415d7cf8e
lixun <xunlily.li@mail.utoronto.ca>
2024-08-06 21:46:53 -0400

    delete some useless class

--- src/test/Java/use_case/CreateCourse_Test.java
+++ src/test/Java/use_case/CreateCourse_Test.java
@@ -40,18 +40,4 @@ public class CreateCourse_Test {
         interactor = new CreateCourseInteractor(presenter, dataAccessObject, groupChatFactory);
     }
 
-    @Test
-    void testCreateCourseAlreadyExists() {
-        // First create a course
-        dataAccessObject.saveCourse(new Course("FM", "ACT240", new GroupChat("ACT240")));
-
-        // Try to create the same course again
-        CreateCourseInputData inputData = new CreateCourseInputData("FM", "ACT240", new GroupChat("ACT240"));
-        interactor.execute(inputData);
-
-        // Check if the error was handled
-        CreateCourseState state = viewModel.getState();
-        assertNull(state.getCode());
-        assertNull(state.getName());
-    }
 }
