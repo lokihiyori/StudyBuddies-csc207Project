1517f2507865f24c679d51bc7ae43a004a45aaea
Yujing Duan <yujing.duan@mail.utoronto.ca>
2024-08-08 02:50:40 -0400

    update with Javadoc

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -1,103 +1,3 @@
-/*
-package view;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-
-import data_access.CourseDataAccessObject;
-import interface_adapter.GoToCourse.CourseViewModel;
-import interface_adapter.GroupChatViewModel;
-import interface_adapter.SearchCourse.SearchCourseController;
-import interface_adapter.SearchCourse.SearchCoursePresenter;
-import interface_adapter.SearchCourse.SearchCourseViewModel;
-import interface_adapter.logged_In.LoggedInState;
-import interface_adapter.logged_In.LoggedInViewModel;
-import interface_adapter.logged_In.LoggedIncontroller;
-import use_case.SearchCourse.SearchCourseInteractor;
-
-public class LoggedInView extends JPanel implements ActionListener, PropertyChangeListener {
-    public final String viewName = "logged in";
-
-    private final JButton searchCourseButton;
-    private final JButton registeredCoursesButton;
-    private final GroupChatViewModel viewModel;
-    private final LoggedInViewModel loggedInViewModel;
-    private final LoggedIncontroller loggedIncontroller;
-    private final CourseViewModel courseViewModel;
-
-    public LoggedInView(GroupChatViewModel viewModel, LoggedInViewModel loggedInViewModel, LoggedIncontroller loggedIncontroller, CourseViewModel courseViewModel) {
-        this.viewModel = viewModel;
-        this.loggedInViewModel = loggedInViewModel;
-        this.loggedInViewModel.addPropertyChangeListener(this);
-        this.loggedIncontroller = loggedIncontroller;
-        this.courseViewModel = courseViewModel;
-        this.courseViewModel.addPropertyChangeListener(this);
-
-        searchCourseButton = new JButton("Go to Search Courses");
-        registeredCoursesButton = new JButton("Go to Your Courses");
-
-        // Set layout manager
-        setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 10, 10, 10);
-
-        // Add search course button
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        add(searchCourseButton, gbc);
-
-        // Add registered courses button
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        add(registeredCoursesButton, gbc);
-
-        searchCourseButton.addActionListener(this);
-        registeredCoursesButton.addActionListener(
-            new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                if (evt.getSource().equals(registeredCoursesButton)) {
-                    LoggedInState currentState = loggedInViewModel.getState();
-                    loggedIncontroller.executeGoToCourse(
-                            currentState.getUsername()
-                    );
-                }
-            }
-            });
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent evt) {
-        if (evt.getSource() == searchCourseButton) {
-            System.out.println("Navigating to Course Search");
-            // Adjust this to manage within current GUI framework
-            //CourseViewModel courseViewModel = new CourseViewModel();
-            JPanel courseSearchPanel = new CourseSearchView(viewModel, new SearchCourseViewModel(), new SearchCourseController(new SearchCourseInteractor(new CourseDataAccessObject(), new SearchCoursePresenter(new SearchCourseViewModel()))), courseViewModel); // Assuming CourseSearchView is a JPanel
-            JOptionPane.showMessageDialog(this, courseSearchPanel, "Course Search", JOptionPane.PLAIN_MESSAGE);
-//         }else if (evt.getSource() == registeredCoursesButton) {
-//            System.out.println("Navigating to Registered Courses");
-//
-//            // Retrieve the current user from the loggedInViewModel
-//            LoggedInState currentState = loggedInViewModel.getState();
-//
-//            loggedIncontroller.executeGoToCourse(
-//                    currentState.getUsername()
-            ;
-        }
-    }
-
-    @Override
-    public void propertyChange(PropertyChangeEvent evt) {
-        LoggedInState state = (LoggedInState) evt.getNewValue();
-        // Update the UI based on state changes
-    }
-}
-
- */
-
 package view;
 
 import javax.swing.*;
@@ -121,6 +21,10 @@ import interface_adapter.logged_In.LoggedIncontroller;
 import interface_adapter.login.LoginState;
 import use_case.SearchCourse.SearchCourseInteractor;
 
+/**
+ * LoggedInView is a JPanel that provides a user interface for users who have logged in.
+ * It allows users to search for courses or view their registered courses.
+ */
 public class LoggedInView extends JPanel implements ActionListener, PropertyChangeListener {
     public final String viewName = "logged in";
 
@@ -130,6 +34,13 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
     private final LoggedInViewModel loggedInViewModel;
     private final LoggedIncontroller loggedIncontroller;
 
+    /**
+     * Constructs a new LoggedInView with the specified view models and controller.
+     *
+     * @param viewModel          the GroupChatViewModel instance
+     * @param loggedInViewModel  the LoggedInViewModel instance
+     * @param loggedIncontroller the LoggedIncontroller instance
+     */
     public LoggedInView(GroupChatViewModel viewModel, LoggedInViewModel loggedInViewModel, LoggedIncontroller loggedIncontroller) {
         this.viewModel = viewModel;
         this.loggedInViewModel = loggedInViewModel;
@@ -168,6 +79,11 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
                 });
     }
 
+    /**
+     * Handles action events for the buttons.
+     *
+     * @param evt the ActionEvent object
+     */
     @Override
     public void actionPerformed(ActionEvent evt) {
         if (evt.getSource() == searchCourseButton) {
@@ -175,24 +91,26 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
             // Adjust this to manage within current GUI framework
             JPanel courseSearchPanel = new CourseSearchView(viewModel, new SearchCourseViewModel(), new SearchCourseController(new SearchCourseInteractor(new CourseDataAccessObject(), new SearchCoursePresenter(new SearchCourseViewModel())))); // Assuming CourseSearchView is a JPanel
             JOptionPane.showMessageDialog(this, courseSearchPanel, "Course Search", JOptionPane.PLAIN_MESSAGE);
-//         }else if (evt.getSource() == registeredCoursesButton) {
-//            System.out.println("Navigating to Registered Courses");
-//
-//            // Retrieve the current user from the loggedInViewModel
-//            LoggedInState currentState = loggedInViewModel.getState();
-//
-//            loggedIncontroller.executeGoToCourse(
-//                    currentState.getUsername()
-            ;
+
         }
     }
 
+    /**
+     * Handles property changes in the view model.
+     *
+     * @param evt the PropertyChangeEvent object
+     */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         LoggedInState state = (LoggedInState) evt.getNewValue();
         // Update the UI based on state changes
     }
 
+    /**
+     * Gets the LoggedIncontroller associated with this view.
+     *
+     * @return the LoggedIncontroller instance
+     */
     public LoggedIncontroller getController() {
         return loggedIncontroller;
     }
