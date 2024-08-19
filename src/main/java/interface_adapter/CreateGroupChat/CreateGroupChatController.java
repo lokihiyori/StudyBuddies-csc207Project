07c5f5ee6512aa121a4c3b31f0ceeb4460dea101
lixun <xunlily.li@mail.utoronto.ca>
2024-08-18 21:31:00 -0400

    add javadoc for interface_adapter-createGroupChat

--- src/main/java/interface_adapter/CreateGroupChat/CreateGroupChatController.java
+++ src/main/java/interface_adapter/CreateGroupChat/CreateGroupChatController.java
@@ -5,12 +5,32 @@ import use_case.CreateGroupChat.CreateGroupChatInputData;
 
 import java.io.IOException;
 
+/**
+ * Controller for handling the creation of a group chat.
+ * It interacts with the use case layer to execute the creation of a group chat
+ * based on the provided input data.
+ */
 public class CreateGroupChatController {
     final CreateGroupChatInputBoundary createGroupChatucInteractor;
+
+
+    /**
+     * Constructs a new CreateGroupChatController with the specified input boundary.
+     *
+     * @param createGroupChatucInteractor the input boundary for creating a group chat
+     */
 
     public CreateGroupChatController(CreateGroupChatInputBoundary createGroupChatucInteractor){
         this.createGroupChatucInteractor = createGroupChatucInteractor;
     }
+    /**
+     * Executes the creation of a group chat with the specified code.
+     * This method creates an instance of {@link CreateGroupChatInputData} with the given code
+     * and passes it to the input boundary for processing.
+     *
+     * @param code the code for the new group chat
+     * @throws IOException if an I/O error occurs during the creation process
+     */
     public void executeCreateGroupChat(String code) throws IOException {
         CreateGroupChatInputData createGroupChatInputData = new CreateGroupChatInputData(code);
         createGroupChatucInteractor.execute(createGroupChatInputData);
--- src/main/java/interface_adapter/CreateGroupChat/CreateGroupChatViewModel.java
+++ src/main/java/interface_adapter/CreateGroupChat/CreateGroupChatViewModel.java
@@ -3,24 +3,47 @@ package interface_adapter.CreateGroupChat;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * ViewModel for managing the state and property change events of the group chat creation process.
+ * This class handles the state of the group chat creation and notifies listeners of any changes.
+ */
 public class CreateGroupChatViewModel{
     private CreateGroupChatState state = new CreateGroupChatState();
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
-
+    /**
+     * Sets the current state of the group chat creation process.
+     * Notifies listeners of the state change.
+     *
+     * @param state the new state to set
+     */
     public void setState(CreateGroupChatState state) {
         CreateGroupChatState oldState = this.state;
         this.state = state;
         support.firePropertyChange("state", oldState, this.state);
     }
 
+    /**
+     * Returns the current state of the group chat creation process.
+     *
+     * @return the current CreateGroupChatState
+     */
     public CreateGroupChatState getState() {
         return state;
     }
 
+    /**
+     * Adds a PropertyChangeListener to receive notifications of state changes.
+     *
+     * @param listener the PropertyChangeListener to add
+     */
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
     }
-
+    /**
+     * Removes a PropertyChangeListener so it no longer receives notifications of state changes.
+     *
+     * @param listener the PropertyChangeListener to remove
+     */
     public void removePropertyChangeListener(PropertyChangeListener listener) {
         support.removePropertyChangeListener(listener);
     }
