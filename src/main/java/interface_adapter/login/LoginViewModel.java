7c72770bf572d30f7d0613a920682ae2a7032aec
lixun <xunlily.li@mail.utoronto.ca>
2024-08-18 21:54:42 -0400

    add javadoc for interface_adapter/login

--- src/main/java/interface_adapter/login/LoginViewModel.java
+++ src/main/java/interface_adapter/login/LoginViewModel.java
@@ -4,6 +4,11 @@ import interface_adapter.ViewModel;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * Represents the ViewModel for the login view. This class manages the state related to user login,
+ * including the username, password, and any error messages. It also supports property change notifications
+ * for updating the view.
+ */
 public class LoginViewModel extends ViewModel {
 
     public final String TITLE_LABEL = "Log In View";
@@ -15,10 +20,18 @@ public class LoginViewModel extends ViewModel {
 
     private LoginState state = new LoginState();
 
+    /**
+     * Constructs a new instance of LoginViewModel with a default view name of "log in".
+     */
     public LoginViewModel() {
         super("log in");
     }
 
+    /**
+     * Sets the state of the LoginViewModel.
+     *
+     * @param state the new state to set
+     */
     public void setState(LoginState state) {
         this.state = state;
     }
@@ -27,14 +40,29 @@ public class LoginViewModel extends ViewModel {
 
     // This is what the Signup Presenter will call to let the ViewModel know
     // to alert the View
+
+    /**
+     * Notifies all registered listeners of changes to the state.
+     * This method should be called when the state changes to update the view.
+     */
     public void firePropertyChanged() {
         support.firePropertyChange("state", null, this.state);
     }
 
+    /**
+     * Adds a property change listener to be notified of state changes.
+     *
+     * @param listener the PropertyChangeListener to add
+     */
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
     }
 
+    /**
+     * Gets the current state of the LoginViewModel.
+     *
+     * @return the current LoginState
+     */
     public LoginState getState() {
         return state;
     }
