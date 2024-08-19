a39d41519e600d30c6cc168f71bdd49c5f4c1235
Yujing Duan <yujing.duan@mail.utoronto.ca>
2024-08-19 12:01:54 -0400

    update CreateEvent use case with Javadoc

--- src/main/java/use_case/CreateEvent/CreateEventInputBoundary.java
+++ src/main/java/use_case/CreateEvent/CreateEventInputBoundary.java
@@ -1,5 +1,19 @@
 package use_case.CreateEvent;
 
+/**
+ * The CreateEventInputBoundary interface defines the method required for handling
+ * the input data when creating a new event. Implementations of this interface
+ * are responsible for processing the provided event input data and executing
+ * the necessary operations to create the event.
+ */
 public interface CreateEventInputBoundary {
+
+    /**
+     * Executes the event creation process using the provided input data.
+     * Implementations should handle the logic for validating and processing the
+     * event data to create a new event in the system.
+     *
+     * @param createEventInputData the data required to create a new event.
+     */
     void execute(CreateEventInputData createEventInputData);
 }
--- src/main/java/use_case/CreateEvent/CreateEventOutputBoundary.java
+++ src/main/java/use_case/CreateEvent/CreateEventOutputBoundary.java
@@ -1,7 +1,19 @@
 package use_case.CreateEvent;
 
+/**
+ * The CreateEventOutputBoundary interface defines the method required for handling
+ * the output of the event creation process. Implementations of this interface
+ * should provide the logic for preparing the view or response when the event creation
+ * process is successful.
+ */
 public interface CreateEventOutputBoundary {
 
-
+    /**
+     * Prepares the view or response to be shown when the event creation process
+     * is successful. Implementations should define how the success information,
+     * including the user data, is presented.
+     *
+     * @param user the data related to the user involved in the event creation process.
+     */
     void prepareSuccessView(CreateEventOutputData user);
 }
