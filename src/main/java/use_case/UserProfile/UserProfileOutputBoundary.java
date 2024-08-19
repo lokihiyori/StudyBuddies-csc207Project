f0ea756f81d69c43ac4cf8dfe9dfc9ea802c0bb8
Yujing Duan <yujing.duan@mail.utoronto.ca>
2024-08-19 13:06:12 -0400

    documentation

--- src/main/java/use_case/UserProfile/UserProfileOutputBoundary.java
+++ src/main/java/use_case/UserProfile/UserProfileOutputBoundary.java
@@ -1,5 +1,15 @@
 package use_case.UserProfile;
 
+/**
+ * The output boundary interface for presenting user profile data.
+ */
 public interface UserProfileOutputBoundary {
+
+    /**
+     * Presents the user profile data.
+     *
+     * @param outputData the data representing the user profile to be presented.
+     * @return the same {@link UserProfileOutputData} that was provided.
+     */
     UserProfileOutputData presentUserProfile(UserProfileOutputData outputData);
 }
