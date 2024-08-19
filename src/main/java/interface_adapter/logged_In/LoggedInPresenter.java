b9570d2671eed9599248b3bbebfba7e232fec9ad
lixun <xunlily.li@mail.utoronto.ca>
2024-08-18 21:46:43 -0400

    javadoc added. /interface_adapter/logged_In

--- src/main/java/interface_adapter/logged_In/LoggedInPresenter.java
+++ src/main/java/interface_adapter/logged_In/LoggedInPresenter.java
@@ -5,13 +5,22 @@ import interface_adapter.GoToCourse.CourseViewModel;
 import interface_adapter.ViewManagerModel;
 import use_case.GoToCourse.GoToCourseOutputBoundary;
 import use_case.GoToCourse.GoToCourseOutputData;
-
+/**
+ * Presenter responsible for managing the view logic when a user is logged in.
+ * It coordinates between the view models and the view manager to update the UI state.
+ */
 public class LoggedInPresenter  {
     private final LoggedInViewModel loggedInViewModel;
     private final CourseViewModel courseViewModel;
 
     private ViewManagerModel viewManagerModel;
-
+    /**
+     * Constructs a new LoggedInPresenter with the specified view models and view manager.
+     *
+     * @param loggedInViewModel the view model for the logged-in state
+     * @param courseViewModel the view model for the course view
+     * @param viewManagerModel the view manager model for managing view transitions
+     */
     public LoggedInPresenter(LoggedInViewModel loggedInViewModel, CourseViewModel courseViewModel, ViewManagerModel viewManagerModel){
         this.loggedInViewModel = loggedInViewModel;
         this.courseViewModel = courseViewModel;
@@ -26,7 +35,9 @@ public class LoggedInPresenter  {
 //        this.viewManagerModel.setActiveView(courseViewModel.getViewName());
 //        this.viewManagerModel.firePropertyChanged();
 //    }
-
+    /**
+     * Prepares the view for a successful login by setting the active view to the logged-in view.
+     */
     public void prepareSuccessView() {
         this.viewManagerModel.setActiveView(loggedInViewModel.getViewName());
         this.viewManagerModel.firePropertyChanged();
--- src/main/java/interface_adapter/logged_In/LoggedIncontroller.java
+++ src/main/java/interface_adapter/logged_In/LoggedIncontroller.java
@@ -3,19 +3,36 @@ package interface_adapter.logged_In;
 import use_case.GoToCourse.GoToCourseInputBoundary;
 import use_case.GoToCourse.GoToCourseInputData;
 import use_case.GoToCourse.GoToCourseInteractor;
-
+/**
+ * Controller for handling actions in the logged-in state related to navigating to a course.
+ * It interacts with the use case for going to a course.
+ */
 public class LoggedIncontroller {
     final GoToCourseInputBoundary goToCourseInteractor;
 
+    /**
+     * Constructs a new LoggedInController with the specified GoToCourseInputBoundary.
+     *
+     * @param goToCourseInputBoundary the input boundary for going to a course
+     */
     public LoggedIncontroller(GoToCourseInputBoundary goToCourseInputBoundary){
         this.goToCourseInteractor = goToCourseInputBoundary;
     }
-
+    /**
+     * Executes the process to navigate to a course for the specified user.
+     *
+     * @param username the username of the user navigating to the course
+     */
     public void executeGoToCourse(String username){
         GoToCourseInputData goToCourseInputData = new GoToCourseInputData(username);
         goToCourseInteractor.execute(goToCourseInputData);
     }
 
+    /**
+     * Gets the GoToCourseInputBoundary used by this controller.
+     *
+     * @return the GoToCourseInputBoundary
+     */
     public Object getGoToCourseInputBoundary() {
         return goToCourseInteractor;
     }
