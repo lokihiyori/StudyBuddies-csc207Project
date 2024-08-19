2e04b66107d1771827bec86127d3512db5e1b0a2
Yujing Duan <yujing.duan@mail.utoronto.ca>
2024-08-19 13:05:43 -0400

    documentation

--- src/main/java/use_case/UserProfile/UserProfileInputData.java
+++ src/main/java/use_case/UserProfile/UserProfileInputData.java
@@ -3,6 +3,9 @@ package use_case.UserProfile;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Represents the data required to create a user profile.
+ */
 public class UserProfileInputData {
     private final String name;
     private final String password;
@@ -10,6 +13,15 @@ public class UserProfileInputData {
     private final LocalDateTime creationTime;
     private final List<String> courseCodes;
 
+    /**
+     * Constructs a UserProfileInputData object with the specified details.
+     *
+     * @param name          the name of the user.
+     * @param password      the password for the user.
+     * @param email         the email address of the user.
+     * @param creationTime  the time when the profile was created.
+     * @param courseCodes   a list of course codes associated with the user.
+     */
     public UserProfileInputData(String name, String password, String email, LocalDateTime creationTime,List<String> courseCodes) {
         this.name = name;
         this.password = password;
@@ -18,22 +30,47 @@ public class UserProfileInputData {
         this.courseCodes = courseCodes;
     }
 
+    /**
+     * Returns the name of the user.
+     *
+     * @return the name of the user.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the password for the user.
+     *
+     * @return the password for the user.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Returns the email address of the user.
+     *
+     * @return the email address of the user.
+     */
     public String getEmail(){
         return email;
     }
 
+    /**
+     * Returns the time when the profile was created.
+     *
+     * @return the creation time of the profile.
+     */
     public LocalDateTime getCreationTime() {
         return creationTime;
     }
 
+    /**
+     * Returns the list of course codes associated with the user.
+     *
+     * @return a list of course codes.
+     */
     public List<String> getCourseCodes() {
         return courseCodes;
     }
