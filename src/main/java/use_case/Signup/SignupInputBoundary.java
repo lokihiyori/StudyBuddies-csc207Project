e3334f7a5544ae7728c1bed736303bfd7af9a794
Yujing Duan <yujing.duan@mail.utoronto.ca>
2024-08-19 12:53:47 -0400

    document Signup

--- src/main/java/use_case/Signup/SignupInputBoundary.java
+++ src/main/java/use_case/Signup/SignupInputBoundary.java
@@ -1,5 +1,16 @@
 package use_case.Signup;
 
+/**
+ * The SignupInputBoundary interface defines the contract for executing the signup process.
+ * It specifies a method to handle user signup input data and perform the necessary operations
+ * for user registration.
+ */
 public interface SignupInputBoundary {
+
+    /**
+     * Executes the signup process using the provided input data.
+     *
+     * @param signupInputData the data required to complete the signup process, including user details.
+     */
     void execute(SignupInputData signupInputData);
 }
