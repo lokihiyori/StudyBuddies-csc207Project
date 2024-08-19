2b4955d8127cab40ec1508bf3c0f0d65d53a7104
lixun <xunlily.li@mail.utoronto.ca>
2024-08-18 22:30:26 -0400

    add javadoc interface_adapter/SignUp

--- src/main/java/interface_adapter/SignUp/SignUpPresenter.java
+++ src/main/java/interface_adapter/SignUp/SignUpPresenter.java
@@ -9,7 +9,10 @@ import use_case.LoginSignup.LoginSignupOutputBoundary;
 import use_case.LoginSignup.LoginSignupOutputData;
 import use_case.Signup.SignupOutputBoundary;
 import use_case.Signup.SignupOutputData;
-
+/**
+ * Presenter for handling user sign-up and login functionality.
+ * This class prepares the view models for success and failure scenarios during sign-up and login processes.
+ */
 public class SignUpPresenter implements SignupOutputBoundary, LoginSignupOutputBoundary {
 
     private final SignUpViewModel signupViewModel;
@@ -17,6 +20,14 @@ public class SignUpPresenter implements SignupOutputBoundary, LoginSignupOutputB
     private ViewManagerModel viewManagerModel;
     private LoginSignupViewModel loginSignupViewModel;
 
+    /**
+     * Constructs a {@code SignUpPresenter} with the specified view models and view manager.
+     *
+     * @param viewManagerModel the view manager model to switch between views
+     * @param signupViewModel the view model for the sign-up view
+     * @param loginViewModel the view model for the login view
+     * @param loginSignupViewModel the view model for the login-signup view
+     */
     public SignUpPresenter(ViewManagerModel viewManagerModel,
                            SignUpViewModel signupViewModel,
                            LoginViewModel loginViewModel,
@@ -27,6 +38,13 @@ public class SignUpPresenter implements SignupOutputBoundary, LoginSignupOutputB
         this.loginSignupViewModel = loginSignupViewModel;
     }
 
+    /**
+     * Prepares the view model for a successful sign-up.
+     * This method updates the login view model with the username from the sign-up response,
+     * then switches the view to the login view.
+     *
+     * @param response the data containing the username from the sign-up process
+     */
     @Override
     public void prepareSuccessView(SignupOutputData response) {
         // On success, switch to the login view.
@@ -40,6 +58,12 @@ public class SignUpPresenter implements SignupOutputBoundary, LoginSignupOutputB
         viewManagerModel.firePropertyChanged();
     }
 
+    /**
+     * Prepares the view model for a failed sign-up.
+     * This method sets the error message in the sign-up view model.
+     *
+     * @param error the error message describing the failure
+     */
     @Override
     public void prepareFailView(String error) {
         SignUpState signupState = signupViewModel.getState();
@@ -47,6 +71,13 @@ public class SignUpPresenter implements SignupOutputBoundary, LoginSignupOutputB
         signupViewModel.firePropertyChanged();
     }
 
+    /**
+     * Prepares the view model for a successful login after sign-up.
+     * This method updates the login view model with the username from the login-signup response,
+     * then switches the view to the login view.
+     *
+     * @param user the data containing the username from the login-signup process
+     */
     public void prepareSuccessView(LoginSignupOutputData user) {
         LoginState loginState = loginViewModel.getState();
         loginState.setUsername(user.getUsername());
--- src/main/java/interface_adapter/SignUp/SignUpState.java
+++ src/main/java/interface_adapter/SignUp/SignUpState.java
@@ -1,5 +1,7 @@
 package interface_adapter.SignUp;
-
+/**
+ * Represents the state of the sign-up process, including user input and validation errors.
+ */
 public class SignUpState {
     private String username = "";
     private String usernameError = null;
@@ -9,7 +11,11 @@ public class SignUpState {
     private String passwordError = null;
     private String repeatPassword = "";
     private String repeatPasswordError = null;
-
+    /**
+     * Constructs a {@code SignUpState} object with the values copied from another {@code SignUpState} instance.
+     *
+     * @param copy the {@code SignUpState} instance to copy values from
+     */
     public SignUpState(SignUpState copy) {
         username = copy.username;
         usernameError = copy.usernameError;
@@ -20,70 +26,152 @@ public class SignUpState {
         repeatPassword = copy.repeatPassword;
         repeatPasswordError = copy.repeatPasswordError;
     }
-
+    /**
+     * Constructs a new {@code SignUpState} object with default values.
+     */
     public SignUpState() {
     }
 
+    /**
+     * Returns the username entered by the user.
+     *
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Returns the error message related to the username input.
+     *
+     * @return the username error message
+     */
     public String getUsernameError() {
         return usernameError;
     }
 
+    /**
+     * Returns the email address entered by the user.
+     *
+     * @return the email address
+     */
     public String getEmail(){
         return email;
     }
 
+    /**
+     * Returns the error message related to the email input.
+     *
+     * @return the email error message
+     */
     public String getEmailError(){
         return emailError;
     }
 
+    /**
+     * Returns the password entered by the user.
+     *
+     * @return the password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Returns the error message related to the password input.
+     *
+     * @return the password error message
+     */
     public String getPasswordError() {
         return passwordError;
     }
 
+    /**
+     * Returns the repeated password entered by the user.
+     *
+     * @return the repeated password
+     */
     public String getRepeatPassword() {
         return repeatPassword;
     }
 
+    /**
+     * Returns the error message related to the repeated password input.
+     *
+     * @return the repeated password error message
+     */
     public String getRepeatPasswordError() {
         return repeatPasswordError;
     }
 
+    /**
+     * Sets the username entered by the user.
+     *
+     * @param username the username to set
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Sets the error message related to the username input.
+     *
+     * @param usernameError the username error message to set
+     */
     public void setUsernameError(String usernameError) {
         this.usernameError = usernameError;
     }
 
+    /**
+     * Sets the email address entered by the user.
+     *
+     * @param email the email address to set
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Sets the error message related to the email input.
+     *
+     * @param emailError the email error message to set
+     */
     public void setEmailError(String emailError) {
         this.emailError = emailError;
     }
 
+    /**
+     * Sets the password entered by the user.
+     *
+     * @param password the password to set
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Sets the error message related to the password input.
+     *
+     * @param passwordError the password error message to set
+     */
     public void setPasswordError(String passwordError) {
         this.passwordError = passwordError;
     }
 
+    /**
+     * Sets the repeated password entered by the user.
+     *
+     * @param repeatPassword the repeated password to set
+     */
     public void setRepeatPassword(String repeatPassword) {
         this.repeatPassword = repeatPassword;
     }
 
+    /**
+     * Sets the error message related to the repeated password input.
+     *
+     * @param repeatPasswordError the repeated password error message to set
+     */
     public void setRepeatPasswordError(String repeatPasswordError) {
         this.repeatPasswordError = repeatPasswordError;
     }
